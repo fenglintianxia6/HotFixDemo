80c88da66dd095b52ec8b6e3f1f4629d48e06f55
郑迎风 <zhengyingfeng@qudian.com>
2016-10-27 14:46:34 +0800

    调通tinker流程 1、打Release包 2、修复bug 3、打patch包 4、调用相关API

--- app/src/main/java/com/dev/hotfixdemo/HotfixApplicationLike.java
+++ app/src/main/java/com/dev/hotfixdemo/HotfixApplicationLike.java
@@ -1,34 +1,47 @@
 package com.dev.hotfixdemo;
 
+import android.annotation.TargetApi;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
+import android.os.Build;
 import android.support.multidex.MultiDex;
 
 import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
 import com.tencent.tinker.loader.app.DefaultApplicationLike;
 import com.tencent.tinker.loader.shareutil.ShareConstants;
 
 /**
  * Created by zyf on 2016/10/26.
  */
-@DefaultLifeCycle(application = "com.dev.hotfixdemo.HotfixApplicaion",
-        flags = ShareConstants.TINKER_ENABLE_ALL, loadVerifyFlag = false)
+@DefaultLifeCycle(application = "com.dev.hotfixdemo.HotFixApplication",//application类名
+        flags = ShareConstants.TINKER_ENABLE_ALL,                 //tinkerFlags
+        loaderClass = "com.tencent.tinker.loader.TinkerLoader",   //loaderClassName, 我们这里使用默认即可!
+        loadVerifyFlag = false)
 public class HotfixApplicationLike extends DefaultApplicationLike {
 
-    public static Application application;
-
-    public HotfixApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent, Resources[] resources, ClassLoader[] classLoader, AssetManager[] assetManager) {
-        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent, resources, classLoader, assetManager);
+    public HotfixApplicationLike(Application application, int tinkerFlags,
+                                 boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime,
+                                 long applicationStartMillisTime, Intent tinkerResultIntent, Resources[] resources,
+                                 ClassLoader[] classLoader, AssetManager[] assetManager) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime,
+                applicationStartMillisTime, tinkerResultIntent, resources, classLoader, assetManager);
     }
 
     @Override
     public void onBaseContextAttached(Context base) {
         super.onBaseContextAttached(base);
-//      you must install multiDex whatever tinker is installed !
+        //      you must install multiDex whatever tinker is installed !
         MultiDex.install(base);
-        application = getApplication();
+        TinkerInstaller.install(this);
     }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public void registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks callback) {
+        getApplication().registerActivityLifecycleCallbacks(callback);
+    }
+
 }
--- app/src/main/java/com/dev/hotfixdemo/MainActivity.java
+++ app/src/main/java/com/dev/hotfixdemo/MainActivity.java
@@ -1,13 +1,46 @@
 package com.dev.hotfixdemo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTest = (TextView) findViewById(R.id.tv_test);
+        setText();
+    }
+
+    public void setText() {
+        tvTest.setText("after hotfix success");
+    }
+
+    public void loadPatch(View view) {
+        int writePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (writePermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+        } else {
+            TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(),
+                    Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk");
+            Log.i("TAG", "loadPatch:path ====  " + Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip.apk");
+            Toast.makeText(this, "load", Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
